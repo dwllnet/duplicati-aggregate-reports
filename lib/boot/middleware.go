b0f3ac4634630e7333401bc1bccda78cbe27c0aa
Daniel Williams <dwilliams@datainventory.com>
2020-12-02 19:59:36 -0500

    Removed CSRF

--- lib/boot/middleware.go
+++ lib/boot/middleware.go
@@ -13,8 +13,8 @@ import (
 // SetUpMiddleware contains the middleware that applies to every request.
 func SetUpMiddleware(h http.Handler) http.Handler {
 	return router.ChainHandler( // Chain middleware, top middleware runs first
-		h,                    // Handler to wrap
-		setUpCSRF,            // Prevent CSRF
+		h, // Handler to wrap
+		//setUpCSRF,            // Prevent CSRF
 		rest.Handler,         // Support changing HTTP method sent via query string
 		logrequest.Handler,   // Log every request
 		context.ClearHandler, // Prevent memory leak with gorilla.sessions
